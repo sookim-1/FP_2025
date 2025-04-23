cb8acfc658cf85ec235bef3565b91ab73f7bf455
sookim-1 <scstnghks@gmail.com>
2025-04-23 22:28:20 +0900

    feat : 도서 검색 화면 UI 구성 (검색창 및 리스트)

--- BookSearchApp/BookSearchApp/ViewController.swift
+++ BookSearchApp/BookSearchApp/ViewController.swift
@@ -7,13 +7,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    private lazy var bookSearchField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.backgroundColor = .systemGray6
+        textField.placeholder = "검색어를 입력해주세요."
+        textField.addTarget(self, action: #selector(bookSearchTextDidChange), for: .editingChanged)
+        return textField
+    }()
+
+    private lazy var bookTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
+
+    private var books: [String] = ["1", "2", "3"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setupViews()
     }
 
+    private func setupViews() {
+        view.backgroundColor = .white
+
+        view.addSubview(bookSearchField)
+        view.addSubview(bookTableView)
+
+        NSLayoutConstraint.activate([
+            bookSearchField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            bookSearchField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            bookSearchField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            bookSearchField.heightAnchor.constraint(equalToConstant: 55),
+
+            bookTableView.topAnchor.constraint(equalTo: bookSearchField.bottomAnchor, constant: 20),
+            bookTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            bookTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            bookTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+
+    @objc private func bookSearchTextDidChange() {
+        print("검색어 : \(bookSearchField.text)")
+    }
+
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return books.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        var content = cell.defaultContentConfiguration()
+        content.text = books[indexPath.row]
+        cell.contentConfiguration = content
+
+        return cell
+    }
 
 }
 
